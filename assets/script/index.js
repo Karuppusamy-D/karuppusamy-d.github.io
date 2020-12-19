e6ab53ab2ca699a4a569461eda112b6b679c8ffa
Karuppusamy <d.karuppusamy@outlook.com>
2020-12-19 14:59:46 +0530

    Added NavBar Overlay to close Side NavBar

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,15 +1,20 @@
 /*===== MENU SHOW =====*/
-const showMenu = (toggleId, navId) => {
-    const toggle = document.getElementById(toggleId),
-        nav = document.getElementById(navId);
+const toggle = document.getElementById('nav-toggle');
+const nav = document.getElementById('nav-menu');
+const nav_overlay = document.querySelector('.nav-overlay');
 
+
+function showMenu() {
     if (toggle && nav) {
         toggle.addEventListener('click', () => {
             nav.classList.toggle('show');
+            toggle.firstElementChild.classList.toggle("bx-x");
+            nav_overlay.classList.toggle("show");
         });
+        nav_overlay.addEventListener('click', () => toggle.click())
     }
 }
-showMenu('nav-toggle', 'nav-menu');
+showMenu();
 
 /*===== ACTIVE AND REMOVE MENU =====*/
 const navLink = document.querySelectorAll('.nav__link');
@@ -18,10 +23,10 @@ function linkAction() {
     /*Active link*/
     navLink.forEach(n => n.classList.remove('active'));
     this.classList.add('active');
-
     /*Remove menu mobile*/
-    const navMenu = document.getElementById('nav-menu');
-    navMenu.classList.remove('show');
+    nav.classList.remove('show');
+    toggle.firstElementChild.classList.remove("bx-x");
+    nav_overlay.classList.remove("show");
 }
 navLink.forEach(n => n.addEventListener('click', linkAction));
 
